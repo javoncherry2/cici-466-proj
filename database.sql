82f5bbe2cd3a7f18679aba9a504d5c9779667d29
javoncherry2 <javoncherry2@gmail.com>
2025-11-23 00:12:23 -0600

    Modify Contributed table structure and add inserts
    Updated primary key for Contributed table to include contributiontype_id. Added multiple insert statements for Contributor and Contributed tables.

--- database.sql
+++ database.sql
@@ -122,7 +122,7 @@ CREATE TABLE Contributed(
     contributor_id INT,
     song_id INT,
     contributiontype_id INT,
-    PRIMARY KEY(contributor_id, song_id),
+    PRIMARY KEY(contributor_id, song_id, contributiontype_id),
     FOREIGN KEY(contributor_id) REFERENCES Contributor(contributor_id)
     ON DELETE CASCADE,
     FOREIGN KEY(song_id) REFERENCES Song(song_id)
@@ -201,6 +201,166 @@ VALUES
     (1, 'Writer'),
     (2, 'Singer'),
     (3, 'instrumentalist');
+
+
+--=================================
+-- Inserts into Contributor ====
+--=================================
+INSERT INTO Contributor 
+VALUES
+(1, 'Abel Tesfaye'),  
+(2, 'Patrick Greenaway'),
+(3, 'Taylor Swift'),
+(4, 'Jack Antonoff'),
+(5, 'Ed Sheeran'),
+(6, 'Drake'),
+(7, 'Bruno Mars'),
+(8, 'Billie Eilish'),
+(9, 'Dan Reynolds'),
+(10, 'Ben McKee'),
+(11, 'Ariana Grande'),
+(12, 'Rihanna'),
+(13, 'Beyoncé'),
+(14, 'Post Malone'),
+(15, 'Harry Styles'),
+(16, 'Paul Epworth'),
+(17, 'Benito Martinez'),
+(18, 'Eminem'),
+(19, 'Chris Martin'),
+(20, 'Jay-Z'),
+(21, 'Jonny Buckland'),
+(22, 'Freddie Mercury'),
+(23, 'Olivia Rodrigo'),
+(24, 'Shakira'),
+(25, 'Dua Lipa'),
+(26, 'Travis Scott'),
+(27, 'Solána Imani Rowe'),
+(28, 'Aubrey Graham'),
+(29, 'Carter Lang'),
+(30, 'Michael Williams II'),
+(31, 'Kendrick Lamar'),
+(32, 'Adele');
+
+--==============================
+-- Inserts into Contributed ====
+--==============================
+INSERT INTO Contributed
+VALUES
+/* The Weeknd */
+(1, 1, 1),
+(1, 1, 2),
+(2, 1, 1),
+(1, 2, 1),
+(1, 2, 2),
+(2, 2, 1),
+
+/* Taylor Swift */
+(3, 3, 1),
+(3, 3, 2),
+(4, 3, 1),
+(3, 4, 1),
+(3, 4, 2),
+(4, 4, 1),
+
+/* Ed Sheeran */
+(5, 5, 1),
+(5, 5, 2),
+(5, 6, 1),
+(5, 6, 2),
+
+/* Drake */
+(6, 7, 1),
+(6, 7, 2),
+(6, 8, 1),
+(6, 8, 2),
+
+/* Bruno Mars */
+(7, 9, 1),
+(7, 9, 2),
+(7, 10, 1),
+(7, 10, 2),
+
+/* Billie Eilish */
+(8, 11, 1),
+(8, 11, 2),
+
+/* Imagine Dragons */
+(9, 12, 2),
+(10, 12, 3),
+(9, 13, 2),
+(10, 13, 3),
+
+/* Ariana Grande */
+(11, 14, 1),
+(11, 14, 2),
+
+/* Rihanna */
+(12, 15, 1),
+(12, 15, 2),
+(12, 16, 1),
+(12, 16, 2),
+(20, 16, 2),
+
+/* Beyoncé */
+(13, 17, 1),
+(13, 17, 2),
+
+/* Post Malone */
+(14, 18, 1),
+(14, 18, 2),
+
+/* Harry Styles */
+(15, 19, 1),
+(15, 19, 2),
+
+/* Ben McKee */
+(17, 20, 1),
+(17, 20, 2),
+
+/* Eminem */
+(18, 21, 1),
+(18, 21, 2),
+
+/* Coldplay */
+(19, 22, 2),
+(21, 22, 3),
+
+/* Queen */
+(22, 23, 2),
+(22, 23, 1),
+
+/* Olivia Rodrigo */
+(23, 24, 1),
+(23, 24, 2),
+
+/* Shakira */
+(24, 25, 1),
+(24, 25, 2),
+
+/* Dua Lipa */
+(25, 26, 1),
+(25, 26, 2),
+
+/* Travis Scott */
+(26, 27, 1),
+(26, 27, 2),
+(28, 27, 3),
+
+/* SZA */
+(27, 28, 1),
+(27, 28, 2),
+(29, 28, 3),
+
+/* Kendrick Lamar */
+(31, 29, 1),
+(31, 29, 2),
+(30, 29, 3),
+
+/* Adele */
+(32, 30, 1),
+(32, 30, 2),
+(16, 30, 3);
+
 
 --===========================
 --Inserts into KaraokeFile ==
